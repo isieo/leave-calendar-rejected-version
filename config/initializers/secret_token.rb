74a586c0f9061a05652a01cdd9a8d42a5395fe5a
SailorMercury <mizunoyukino@hotmail.com>
2012-02-11 17:06:44 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeaveCalendar::Application.config.secret_token = '01c9ae8fb79ee32fd58b39b4f2a7cbbb53ef23f234761b8c5d4506c451f8f1575232bf712c23fa80be3c86dc456f21f59a70012108850309d16104c16b333e0a'
